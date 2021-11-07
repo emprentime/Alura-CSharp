4f8ab5c5b5121483586aa9b33f73507d0a077fc1
Marcio Roberto Leite da Silva <emprentime@gmail.com>
2021-11-07 18:41:55 -0300

    Primeiro programa (Olá, Mundo!) com C#, onde aprendemos exibir uma mensagem na tela do console.

--- 1-PrimeiroProjeto/Program.cs
+++ 1-PrimeiroProjeto/Program.cs
@@ -5,9 +5,12 @@ namespace _1_PrimeiroProjeto
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // Aula 01
+
             // O comando abaixo serve para exibirmos alguma mensagem para o usuário.
             Console.WriteLine("Olá, Mundo! Este é o meu primeiro programa C# no Visual Studio.");
+
             Console.WriteLine("A execução acabou. tecle enter para finalizar...");
             // O comando abaixo serve para lermos alguma informação que o usuário digita na tela.
             Console.ReadLine();
