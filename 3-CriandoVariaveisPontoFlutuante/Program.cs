35e264d9732c4205ad56c732c1e615f0ff63f3f4
Marcio Roberto Leite da Silva <emprentime@gmail.com>
2021-11-07 19:21:21 -0300

    Trabalhamos com variáveis tipo double (pontos flutuantes - números que usam vírgulas (como 1,5), ou seja, números quebrados.)

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Projeto 3
+            Console.WriteLine("Executando projeto 3 Criando variáveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15.0 / 2;
+
+            Console.WriteLine(idade); // resultado 7.5
+
+            idade = 5 / 3;
+            Console.WriteLine("5 / 3 = " + idade); // resultado 1
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0 / 3 = " + idade); // resultado 1,6666666666666667
+
+            Console.WriteLine("A execução acabou. tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
