f258ed7a2a528711b43ce83a92dd53671faf17e0
Marcio Roberto Leite da Silva <emprentime@gmail.com>
2021-11-07 23:39:50 -0300

    Aprendemos um pouco sobre a estrutura condicional if/else.

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Projeto 7
+            Console.WriteLine("Executando projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João possui mais de 18 anos de idade. Pode entrar.");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado. Pode entrar");
+                }
+                else
+                {
+                    Console.WriteLine("João não possui mais de 18 anos, não pode entrar");
+                }
+            }
+
+            Console.WriteLine("A execução acabou. tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
